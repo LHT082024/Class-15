b67644367e3a6b47cfffcb828d937b5467cd8b6f
SapphireShy <lucielity@gmail.com>
2024-04-09 11:38:57 +0200

    numbers

---
+++ java2.js
@@ -0,0 +1,20 @@
+
+
+let number = 8;
+
+if (number === 10) {
+     console.log("dette tallet er akkurat 10");
+} 
+
+else if (number < 10 ){
+     console.log ("dette tallet er mindre en 10");
+}
+
+else if (number > 10){
+     console.log ("dette tallet er større en 10");
+}
+
+else {
+     console.log("Dette er ikke et tall");
+} 
+
