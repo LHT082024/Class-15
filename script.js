5dfdc3c67d1c2b71c15fb81bc0ebbf12dd9de4e9
SapphireShy <lucielity@gmail.com>
2024-04-09 10:42:35 +0200

    stuff

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('Hello!');
+
+const submittValue = document.querySelector("submittValue");
+console.log(submittValue);
+
+submittValue.addEventListener("click", () => {
+    const inputValue = document.querySelector("inputValue").value;
+    console.log(inputValue);
+
+});
