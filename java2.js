dd5d13ac3cd70f5f9cdadcc0e8725fda2e9b1c8e
SapphireShy <lucielity@gmail.com>
2024-04-09 11:43:53 +0200

    s

--- java2.js
+++ java2.js
@@ -5,15 +5,12 @@ let number = 8;
 if (number === 10) {
      console.log("dette tallet er akkurat 10");
 } 
-
-else if (number < 10 ){
+else if (number < 10 ) {
      console.log ("dette tallet er mindre en 10");
 }
-
-else if (number > 10){
+else if (number > 10) {
      console.log ("dette tallet er større en 10");
 }
-
 else {
      console.log("Dette er ikke et tall");
 } 
